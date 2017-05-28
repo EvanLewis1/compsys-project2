c9457a08d8cdec10d92c7663baf38ab6a133a6d7
EvanLewis1 <evancharleslewis@me.com>
2017-05-28 20:45:21 +1000

    Add files via upload
    Very basic reverse class
    Added choice of update based on state

--- MyAIController.java
+++ MyAIController.java
@@ -9,11 +9,13 @@ public class MyAIController extends CarController{
 	public int loop = 1;
 	public String startPosition;
 	public String prevPosition;
+	public String state;
 
 	public MyAIController(Car car) {
 		super(car);
 		this.prevPosition  = car.getPosition();
 		this.startPosition = car.getPosition();
+		this.state = "wallFollower";
 		
 		this.wallFollower = new WallFollower(this);
 	}
@@ -26,7 +28,27 @@ public class MyAIController extends CarController{
 			
 		}
 		
-		wallFollower.update(delta);
+		if(state.equals("wallFollower")){
+			wallFollower.update(delta);
+		}
+		else if (state.equals("Reverse")){
+			Reverse.update(this);
+		}
+		else if(state.equals("3PointTurn")){
+			3PointTurn.update(this);
+		}
+		else if(state.equals("UTurn")){
+			UTurn.update();
+		}
+		else if (state.equals("Lava")){
+			LavaHandler.update(this);
+		}
+		else if(state.equals("Mud")){
+			MudHandler.update(this);
+		}
+		else if(state.equals("Grass")){
+			GrassHandler.update();
+		}
 		
 	}
 
---
+++ Reverse.java
@@ -0,0 +1,13 @@
+package mycontroller;
+
+public class Reverse {
+	
+	static void update(MyAIController controller){
+		
+		if(controller.getVelocity() > 1);
+		controller.applyReverseAcceleration();
+		
+		
+	}
+
+}
