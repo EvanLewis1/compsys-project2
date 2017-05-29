17a227eb2538178267367995a296ecf193ba66c9
EvanLewis1 <evancharleslewis@me.com>
2017-05-29 13:28:00 +1000

    Add files via upload
    More state choice logic

--- MyAIController.java
+++ MyAIController.java
@@ -1,6 +1,5 @@
 package mycontroller;
 
-
 import java.util.HashMap;
 
 import tiles.GrassTrap;
@@ -13,7 +12,7 @@ import world.Car;
 import world.WorldSpatial;
 import world.WorldSpatial.Direction;
 
-public class MyAIController extends CarController{
+public class MyAIController extends CarController {
 	WallFollower wallFollower;
 	public int loop = 1;
 	public String startPosition;
@@ -23,87 +22,148 @@ public class MyAIController extends CarController{
 
 	public MyAIController(Car car) {
 		super(car);
-		this.prevPosition  = car.getPosition();
+		this.prevPosition = car.getPosition();
 		this.startPosition = car.getPosition();
 		this.state = "wallFollower";
-		
+
 		this.wallFollower = new WallFollower(this);
 	}
 
 	@Override
 	public void update(float delta) {
-		if (loopFinished()){
+		if (loopFinished()) {
 			loop++;
 			System.out.println(loop);
-			
+
 		}
-		
+
 		resetstate();
-		
-		if(state.equals("wallFollower")){
+
+		if (state.equals("wallFollower")) {
 			wallFollower.update(delta);
-		}
-		else if (state.equals("Reverse")){
+		} else if (state.equals("Reverse")) {
 			Reverse.update(this, delta);
-		}
-		else if(state.equals("3PointTurn")){
+		} else if (state.equals("3PointTurn")) {
 			ThreePointTurn.update(this, delta);
-		}
-		else if(state.equals("UTurn")){
+		} else if (state.equals("UTurn")) {
 			UTurn.update(this, delta);
-		}
-		else if (state.equals("Lava")){
+		} else if (state.equals("Lava")) {
 			LavaHandler.update(this, delta);
-		}
-		else if(state.equals("Mud")){
+		} else if (state.equals("Mud")) {
 			MudHandler.update(this, delta);
-		}
-		else if(state.equals("Grass")){
+		} else if (state.equals("Grass")) {
 			GrassHandler.update(this, delta);
 		}
-		
+
 	}
 
 	private void resetstate() {
-		
+
 		HashMap<Coordinate, MapTile> currentView = getView();
 
-		if (!checkWallAhead(this.getOrientation(), currentView)){
+		if (!checkWallAhead(this.getOrientation(), currentView)) {
 			this.state = "wallFollower";
-			
+
 		}
-		
-		if(loop > 1){
-			if (getTileAhead() instanceof LavaTrap){
+
+		if (loop > 1) {
+			if (getTileAhead() instanceof LavaTrap) {
 				System.out.println("Lava");
 				this.state = "Lava";
-				
-			}
-			else if (getTileAhead() instanceof MudTrap){
+
+			} else if (getTileAhead() instanceof MudTrap) {
 				System.out.println("Mud");
 				this.state = "Mud";
-				
-			}
-			else if (getTileAhead() instanceof GrassTrap){
+
+			} else if (getTileAhead() instanceof GrassTrap) {
 				System.out.println("Grass");
 				this.state = "Grass";
+			}
+
+		}
+		// Surrounded by walls dead end -> reverse
+		if (getTileAhead().getName().equals("Wall") || loop == 1 && getTileAhead().getName().equals("Trap")) {
+			// Choose escape method
+
+			this.state = "UTurn";
+
+			if (getTileLeft().getName().equals("Wall")
+					|| loop == 1 && getTileLeft().getName().equals("Trap") && getTileRight().getName().equals("Wall")
+					|| loop == 1 && getTileRight().getName().equals("Trap")) {
+				this.state = "Reverse";
+
+			}
+			if (loop == 1) {
+				if (getTileAhead().getName().equals("Wall")) {
+					if (getTileLeft().getName().equals("Wall") || getTileRight().getName().equals("Wall")) {
+						this.state = "ThreePointTurn";
+
+					}
+
 				}
-					
+			}
+
 		}
-		
-		if(getTileAhead().getName().equals("Wall") || loop == 1 && getTileAhead().getName().equals("Trap")){
-			
-			
-		}
-		
-		
+
 	}
-	
+
+	private MapTile getTileRight() {
+		Direction orientation = this.getOrientation();
+		HashMap<Coordinate, MapTile> currentView = this.getView();
+		Coordinate currentPosition;
+		switch (orientation) {
+		case EAST:
+			currentPosition = new Coordinate(this.getPosition());
+			currentPosition.y = currentPosition.y + 1;
+			return getTile(currentView, currentPosition);
+		case NORTH:
+			currentPosition = new Coordinate(this.getPosition());
+			currentPosition.x = currentPosition.x + 1;
+			return getTile(currentView, currentPosition);
+		case SOUTH:
+			currentPosition = new Coordinate(this.getPosition());
+			currentPosition.x = currentPosition.x - 1;
+			return getTile(currentView, currentPosition);
+		case WEST:
+			currentPosition = new Coordinate(this.getPosition());
+			currentPosition.y = currentPosition.y - 1;
+			return getTile(currentView, currentPosition);
+		default:
+			return null;
+		}
+	}
+
+	private MapTile getTileLeft() {
+		Direction orientation = this.getOrientation();
+		HashMap<Coordinate, MapTile> currentView = this.getView();
+		Coordinate currentPosition;
+		switch (orientation) {
+		case EAST:
+			currentPosition = new Coordinate(this.getPosition());
+			currentPosition.y = currentPosition.y - 1;
+			return getTile(currentView, currentPosition);
+		case NORTH:
+			currentPosition = new Coordinate(this.getPosition());
+			currentPosition.x = currentPosition.x - 1;
+			return getTile(currentView, currentPosition);
+		case SOUTH:
+			currentPosition = new Coordinate(this.getPosition());
+			currentPosition.x = currentPosition.x + 1;
+			return getTile(currentView, currentPosition);
+		case WEST:
+			currentPosition = new Coordinate(this.getPosition());
+			currentPosition.y = currentPosition.y + 1;
+			return getTile(currentView, currentPosition);
+		default:
+			return null;
+		}
+	}
+
 	private MapTile getTileAhead() {
 		Direction orientation = this.getOrientation();
 		HashMap<Coordinate, MapTile> currentView = this.getView();
 		Coordinate currentPosition;
-		switch(orientation){
+		switch (orientation) {
 		case EAST:
 			currentPosition = new Coordinate(this.getPosition());
 			currentPosition.x = currentPosition.x + 1;
@@ -118,11 +178,11 @@ public class MyAIController extends CarController{
 			return getTile(currentView, currentPosition);
 		case WEST:
 			currentPosition = new Coordinate(this.getPosition());
-			currentPosition.x = currentPosition.x-1;
+			currentPosition.x = currentPosition.x - 1;
 			return getTile(currentView, currentPosition);
 		default:
 			return null;
-		
+
 		}
 	}
 
@@ -133,12 +193,15 @@ public class MyAIController extends CarController{
 
 	/**
 	 * Check if you have a wall in front of you!
-	 * @param orientation the orientation we are in based on WorldSpatial
-	 * @param currentView what the car can currently see
+	 * 
+	 * @param orientation
+	 *            the orientation we are in based on WorldSpatial
+	 * @param currentView
+	 *            what the car can currently see
 	 * @return
 	 */
-	public boolean checkWallAhead(WorldSpatial.Direction orientation, HashMap<Coordinate, MapTile> currentView){
-		switch(orientation){
+	public boolean checkWallAhead(WorldSpatial.Direction orientation, HashMap<Coordinate, MapTile> currentView) {
+		switch (orientation) {
 		case EAST:
 			return checkEast(currentView);
 		case NORTH:
@@ -149,80 +212,83 @@ public class MyAIController extends CarController{
 			return checkWest(currentView);
 		default:
 			return false;
-		
+
 		}
 	}
 
 	private boolean loopFinished() {
-		if(!this.prevPosition.equals(this.getPosition())){
-			if(this.startPosition.equals(this.getPosition())){
+		if (!this.prevPosition.equals(this.getPosition())) {
+			if (this.startPosition.equals(this.getPosition())) {
 				this.prevPosition = this.getPosition();
 				return true;
 			}
-			this.prevPosition = this.getPosition();	
+			this.prevPosition = this.getPosition();
 		}
-		
+
 		return false;
 	}
-	
 
 	/**
-	 * Method below just iterates through the list and check in the correct coordinates.
-	 * i.e. Given your current position is 10,10
-	 * checkEast will check up to wallSensitivity amount of tiles to the right.
-	 * checkWest will check up to wallSensitivity amount of tiles to the left.
-	 * checkNorth will check up to wallSensitivity amount of tiles to the top.
-	 * checkSouth will check up to wallSensitivity amount of tiles below.
+	 * Method below just iterates through the list and check in the correct
+	 * coordinates. i.e. Given your current position is 10,10 checkEast will
+	 * check up to wallSensitivity amount of tiles to the right. checkWest will
+	 * check up to wallSensitivity amount of tiles to the left. checkNorth will
+	 * check up to wallSensitivity amount of tiles to the top. checkSouth will
+	 * check up to wallSensitivity amount of tiles below.
 	 */
-	public boolean checkEast(HashMap<Coordinate, MapTile> currentView){
+	public boolean checkEast(HashMap<Coordinate, MapTile> currentView) {
 		// Check tiles to my right
 		Coordinate currentPosition = new Coordinate(getPosition());
-		for(int i = 0; i <= wallSensitivity; i++){
-			MapTile tile = currentView.get(new Coordinate(currentPosition.x+i, currentPosition.y));
-		//	System.out.println(tile.getName());
-			if(tile.getName().equals("Wall") || loop == 1 && tile.getName().equals("Trap")){// || loop == 1 && tile.){
+		for (int i = 0; i <= wallSensitivity; i++) {
+			MapTile tile = currentView.get(new Coordinate(currentPosition.x + i, currentPosition.y));
+			// System.out.println(tile.getName());
+			if (tile.getName().equals("Wall") || loop == 1 && tile.getName().equals("Trap")) {// ||
+																								// loop
+																								// ==
+																								// 1
+																								// &&
+																								// tile.){
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public boolean checkWest(HashMap<Coordinate,MapTile> currentView){
+
+	public boolean checkWest(HashMap<Coordinate, MapTile> currentView) {
 		// Check tiles to my left
 		Coordinate currentPosition = new Coordinate(getPosition());
-		for(int i = 0; i <= wallSensitivity; i++){
-			MapTile tile = currentView.get(new Coordinate(currentPosition.x-i, currentPosition.y));
-			if(tile.getName().equals("Wall") || loop == 1 && tile.getName().equals("Trap")){
+		for (int i = 0; i <= wallSensitivity; i++) {
+			MapTile tile = currentView.get(new Coordinate(currentPosition.x - i, currentPosition.y));
+			if (tile.getName().equals("Wall") || loop == 1 && tile.getName().equals("Trap")) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public boolean checkNorth(HashMap<Coordinate,MapTile> currentView){
+
+	public boolean checkNorth(HashMap<Coordinate, MapTile> currentView) {
 		// Check tiles to towards the top
 		Coordinate currentPosition = new Coordinate(getPosition());
-		for(int i = 0; i <= wallSensitivity; i++){
-			MapTile tile = currentView.get(new Coordinate(currentPosition.x, currentPosition.y+i));
-			if(tile.getName().equals("Wall")|| (loop == 1 && tile.getName().equals("Trap"))){
+		for (int i = 0; i <= wallSensitivity; i++) {
+			MapTile tile = currentView.get(new Coordinate(currentPosition.x, currentPosition.y + i));
+			if (tile.getName().equals("Wall") || (loop == 1 && tile.getName().equals("Trap"))) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public boolean checkSouth(HashMap<Coordinate,MapTile> currentView){
+
+	public boolean checkSouth(HashMap<Coordinate, MapTile> currentView) {
 		// Check tiles towards the bottom
 		Coordinate currentPosition = new Coordinate(getPosition());
-		for(int i = 0; i <= wallSensitivity; i++){
-			MapTile tile = currentView.get(new Coordinate(currentPosition.x, currentPosition.y-i));
-			if(tile.getName().equals("Wall")|| loop == 1 && tile.getName().equals("Trap")){
-				
+		for (int i = 0; i <= wallSensitivity; i++) {
+			MapTile tile = currentView.get(new Coordinate(currentPosition.x, currentPosition.y - i));
+			if (tile.getName().equals("Wall") || loop == 1 && tile.getName().equals("Trap")) {
+
 				return true;
 			}
 		}
 		return false;
 	}
-	
 
 }
